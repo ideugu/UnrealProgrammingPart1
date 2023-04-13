7f52c917655cdc1ae9f60c78bca26858704f3aa5
dustin lee <guru@dustinlee.me>
2023-04-13 20:23:36 +0900

    헬로 언리얼 소스코드

---
+++ Source/HelloUnreal/HelloUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HelloUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOUNREAL_API AHelloUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/HelloUnreal/MyGameInstance.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogTemp, Log, TEXT("Hello Unreal!"));
+
+}
