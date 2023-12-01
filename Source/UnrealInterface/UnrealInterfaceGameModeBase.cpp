740cfa3740424d24719b0a38fa1f302f805d05b5
ideugu <guru@dustinlee.me>
2023-12-01 19:57:31 +0900

    헤더파일 오류 수정

--- Source/UnrealInterface/UnrealInterfaceGameModeBase.cpp
+++ Source/UnrealInterface/UnrealInterfaceGameModeBase.cpp
@@ -1,6 +1,4 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
+#include "UnrealInterfaceGameModeBase.h"
 
-#include "LessonInterface.h"
-
-// Add default functionality here for any ILessonInterface functions that are not pure virtual.
