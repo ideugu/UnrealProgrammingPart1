8cb4935b780efc1659077ab65abd803de9f2733e
ideugu <guru@dustinlee.me>
2023-12-13 10:13:17 +0900

    CommonUtility 플러그인 모듈 추가 ( 최종 예제용 )

--- Source/UnrealBuildSystem/UnrealBuildSystem.Build.cs
+++ Source/UnrealBuildSystem/UnrealBuildSystem.Build.cs
@@ -8,7 +8,7 @@ public class UnrealBuildSystem : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "CommonUtility" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
