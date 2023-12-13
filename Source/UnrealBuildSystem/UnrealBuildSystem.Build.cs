f0e64bdeda902833d22af97fb3771f9bd76cbbd1
ideugu <guru@dustinlee.me>
2023-12-13 10:07:33 +0900

    불필요한 모듈 제거

--- Source/UnrealBuildSystem/UnrealBuildSystem.Build.cs
+++ Source/UnrealBuildSystem/UnrealBuildSystem.Build.cs
@@ -8,7 +8,7 @@ public class UnrealBuildSystem : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "CommonUtility" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
