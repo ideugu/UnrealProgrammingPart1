ab22078b432aa0044cb66f990c576b4d8ab22e48
ideugu <guru@dustinlee.me>
2024-08-23 00:20:24 +0900

    5.4 업그레이드

--- Source/UnrealBuildSystem.Target.cs
+++ Source/UnrealBuildSystem.Target.cs
@@ -8,8 +8,8 @@ public class UnrealBuildSystemTarget : TargetRules
 	public UnrealBuildSystemTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("UnrealBuildSystem");
 	}
 }
