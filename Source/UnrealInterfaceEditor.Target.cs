6f859bb41301af55fb10ea3f040b8112ecfea3ba
ideugu <guru@dustinlee.me>
2024-08-23 00:21:48 +0900

    5.4 업그레이드

--- Source/UnrealInterfaceEditor.Target.cs
+++ Source/UnrealInterfaceEditor.Target.cs
@@ -8,8 +8,8 @@ public class UnrealInterfaceEditorTarget : TargetRules
 	public UnrealInterfaceEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("UnrealInterface");
 	}
 }
