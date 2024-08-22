be3df6b979d38bd20f3d22a4af7501ef67f11659
ideugu <guru@dustinlee.me>
2024-08-23 00:03:06 +0900

    5.4버전 업그레이드

--- Source/HelloUnreal.Target.cs
+++ Source/HelloUnreal.Target.cs
@@ -8,8 +8,8 @@ public class HelloUnrealTarget : TargetRules
 	public HelloUnrealTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("HelloUnreal");
 	}
 }
