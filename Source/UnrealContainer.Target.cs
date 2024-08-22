38c42c87fa8fb9ec6f28407163f4899cc8cf9500
ideugu <guru@dustinlee.me>
2024-08-23 00:21:16 +0900

    5.4 업그레이드

--- Source/UnrealContainer.Target.cs
+++ Source/UnrealContainer.Target.cs
@@ -8,8 +8,8 @@ public class UnrealContainerTarget : TargetRules
 	public UnrealContainerTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("UnrealContainer");
 	}
 }
--- Source/UnrealContainerEditor.Target.cs
+++ Source/UnrealContainerEditor.Target.cs
@@ -8,8 +8,8 @@ public class UnrealContainerEditorTarget : TargetRules
 	public UnrealContainerEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("UnrealContainer");
 	}
 }
