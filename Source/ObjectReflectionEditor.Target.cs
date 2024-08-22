8c1e63de220deb409cfe223ba2d236d95d0b9887
ideugu <guru@dustinlee.me>
2024-08-23 00:21:59 +0900

    5.4 업그레이드

--- Source/ObjectReflectionEditor.Target.cs
+++ Source/ObjectReflectionEditor.Target.cs
@@ -8,8 +8,8 @@ public class ObjectReflectionEditorTarget : TargetRules
 	public ObjectReflectionEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("ObjectReflection");
 	}
 }
